f8089f29d879c025daa67d079028f7b2cceedad4
sttr19 <stastroshko@gmail.com>
2023-01-03 00:46:57 +0300

    feat: generateOdds task done

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -35,8 +35,10 @@ function findElement(arr, value) {
  *    2 => [ 1, 3 ]
  *    5 => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  const arr = new Array(len).fill(0);
+  const result = arr.map((e, i) => 2 * i + 1);
+  return result;
 }
 
 /**
